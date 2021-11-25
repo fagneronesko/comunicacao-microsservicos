f104d88fd1617a25cadd7e4f7e481e39b8b48a52
Fagner Onesko <fagner@fagner.fagner>
2021-11-25 20:59:51 -0300

    Adiciona configuracao mongodb sales-api

--- sales-api/app.js
+++ sales-api/app.js
@@ -1,10 +1,14 @@
 import express from "express";
 
+import { connect } from "./src/config/db/MongoDbConfig.js";
+
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
 
-app.get('/api/status', (req, res) => {
+connect();
+
+app.get("/api/status", (req, res) => {
     return res.status(200).json({
         service: "Sales-API",
         status: "up",
